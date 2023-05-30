bd8580ea922b0d1f122b541b2814eb9f9a28637a
Julia Teixeira Barbosa <juliateixeira22@hotmail.com>
2023-05-30 11:58:56 -0300

    menu-lateral

--- notificacoes.js
+++ notificacoes.js
@@ -1,14 +1,15 @@
-function openNav(){
-    document.getElementById('myNav').style.width='270px'
+var menu = document.getElementById('botao-open')
+
+var container = document.querySelector('.container-icones')
+
+var notif = document.querySelector('.icon-notif')
+
+menu.onclick = e => {
+    if(!container.classList.contains('oculto')|| !notif.classList.contains('oculto')){
+       container.classList.add('oculto')
+       notif.classList.add('oculto')
+    } else {
+        container.classList.remove('oculto')
+        notif.classList.remove('oculto')
+    }
 }
-
-function closeNav(){
-    document.getElementById('myNav').style.width='0%'
-}
-
-
-
-/* 
-botao-open = open-menu
-id= myNav / class= overflay == id= menu  /
-*/
